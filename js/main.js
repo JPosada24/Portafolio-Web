1e0056dc7fbf9ce14eb20422f081b2ad4876adbb
Julian Posada <julian_2120@hotmail.com>
2023-07-13 19:41:26 -0500

    Actualización de diseño

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-let set_theme = document.getElementById('body');
-let checkbox = document.getElementById('toggle-checkbox');
-let header = document.getElementById('header');
-let logo = document.querySelector('.logo-nav');
+// let set_theme = document.getElementById('body');
+// let checkbox = document.getElementById('toggle-checkbox');
+// let header = document.getElementById('header');
+// let logo = document.querySelector('.logo-nav');
 
-checkbox.addEventListener("change", validarCheckbox, false);
+// checkbox.addEventListener("change", validarCheckbox, false);
 
-function validarCheckbox()
-{
-    let checked = checkbox.checked;
+// function validarCheckbox()
+// {
+//     let checked = checkbox.checked;
 
-    if (checked)
-    {
-        set_theme.classList.add('night');
-        header.classList.add('header-dark');
-        logo.src = "img/Logo-Dev-light.svg";
-    }
-    else
-    {
-        set_theme.classList.remove('night');
-        header.classList.remove('header-dark');
-        logo.src = "img/Logo-Dev.svg";
-    }
-};
+//     if (checked)
+//     {
+//         set_theme.classList.add('night');
+//         header.classList.add('header-dark');
+//         logo.src = "img/Logo-Dev-light.svg";
+//     }
+//     else
+//     {
+//         set_theme.classList.remove('night');
+//         header.classList.remove('header-dark');
+//         logo.src = "img/Logo-Dev.svg";
+//     }
+// };
 
 date = new Date();
 year = date.getFullYear();
